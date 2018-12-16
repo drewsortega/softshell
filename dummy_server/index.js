3787ead429984eba9845751f3ed1777ab3728990
Drew S. Ortega <DrewSOrtega@gmail.com>
2018-12-16 13:48:48 -0800

    add cors to dummy server

--- dummy_server/index.js
+++ dummy_server/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var example_json = require('./example');
+var cors = require('cors');
+app.use(cors());
 
 app.get('/api/twitter', function (req, res) {
     res.status(200).json(example_json);
