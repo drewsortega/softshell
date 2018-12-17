a28721df8018dd2c1dbb6af28148d81fdc0f5785
uekyq <43017592+uekyq@users.noreply.github.com>
2018-12-16 16:35:08 -0800

    changed javascript in handling form submissions and started work on animation triggers

--- dummy_server/index.js
+++ dummy_server/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var example_json = require('./example');
 var cors = require('cors');
+var morgan = require('morgan');
 app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/api/twitter', function (req, res) {
     res.status(200).json(example_json);
