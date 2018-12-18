64106de99431646501e4a51d3bc3113f2192f4ec
Drew S. Ortega <DrewSOrtega@gmail.com>
2018-12-18 00:57:17 -0800

    update dummy server to reflect proper output format

--- dummy_server/example.js
+++ dummy_server/example.js
@@ -2,28 +2,88 @@ module.exports = {
     validURL: true,
     'data': {
         'top10bad': [
-            'badw0',
-            'badw1',
-            'badw2',
-            'badw3',
-            'badw4',
-            'badw5',
-            'badw6',
-            'badw7',
-            'badw8',
-            'badw9',
+            {
+                "text": 'badw0',
+                count: 5999
+            },
+            {
+                "text": 'badw1',
+                count: 599
+            },
+            {
+                "text": 'badw2',
+                count: 420
+            },
+            {
+                "text": 'badw3',
+                count: 350
+            },
+            {
+                "text": 'badw4',
+                count: 39
+            },
+            {
+                "text": 'badw5',
+                count: 30
+            },
+            {
+                "text": 'badw6',
+                count: 29
+            },
+            {
+                "text": 'badw7',
+                count: 28
+            },
+            {
+                "text": 'badw8',
+                count: 22
+            },
+            {
+                "text": 'badw9',
+                count: 12
+            },
         ],
         'top10good': [
-            'goodw0',
-            'goodw1',
-            'goodw2',
-            'goodw3',
-            'goodw4',
-            'goodw5',
-            'goodw6',
-            'goodw7',
-            'goodw8',
-            'goodw9',
+            {
+                "text": 'goodw0',
+                count: 70000
+            },
+            {
+                "text": 'goodw1',
+                count: 1231
+            },
+            {
+                "text": 'goodw2',
+                count: 123
+            },
+            {
+                "text": 'goodw3',
+                count: 120
+            },
+            {
+                "text": 'goodw4',
+                count: 101
+            },
+            {
+                "text": 'goodw5',
+                count: 89
+            },
+            {
+                "text": 'goodw6',
+                count: 88
+            },
+            {
+                "text": 'goodw7',
+                count: 75
+            },
+            {
+                "text": 'goodw8',
+                count: 74
+            },
+            {
+                "text": 'goodw9',
+                count: 2
+            },
         ],
         'percentages': {
             'percentNaughty': 15,
